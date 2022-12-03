be544964d571c29e3d3ab4a810e0fbb39e7b405c
Ярослав <umpepatopChannel@gmail.com>
2022-12-03 16:31:56 +0500

    Add Circle and Triangle

---
+++ AreaLib/Shapes/Circle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaLib.Shapes
+{
+    internal class Circle : IShape
+    {
+        public double Area(double[] shapeParams)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
