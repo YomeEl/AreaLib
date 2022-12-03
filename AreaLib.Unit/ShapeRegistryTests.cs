c0a4110ea11c327bb0ea8a058614d90e392f32c5
Ярослав <umpepatopChannel@gmail.com>
2022-12-03 17:48:07 +0500

    Fix arguments

--- AreaLib.Unit/ShapeRegistryTests.cs
+++ AreaLib.Unit/ShapeRegistryTests.cs
@@ -21,11 +21,11 @@ namespace AreaLib.Unit
 
             registry.RegisterShape<TestShape>();
             Assert.That(registry.GetAvailableShapes()[0], Is.EqualTo("TestShape"));
-            Assert.That(registry.GetArea("TestShape"), Is.EqualTo(-1.0d));
+            Assert.That(registry.GetArea("TestShape", Array.Empty<double>()), Is.EqualTo(-1.0d));
             
             registry.RegisterShape<TestShape>("OtherName");
             Assert.That(registry.GetAvailableShapes()[1], Is.EqualTo("OtherName"));
-            Assert.That(registry.GetArea("OtherName"), Is.EqualTo(-1.0d));
+            Assert.That(registry.GetArea("OtherName", Array.Empty<double>()), Is.EqualTo(-1.0d));
         }
     }
 }
