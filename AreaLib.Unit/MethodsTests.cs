d0acd10812ad19634e9d0751a725c16f15a32d21
Ярослав <umpepatopChannel@gmail.com>
2022-12-03 19:00:18 +0500

    Restructure tests

--- AreaLib.Unit/MethodsTests.cs
+++ AreaLib.Unit/MethodsTests.cs
@@ -13,52 +13,11 @@ namespace AreaLib.Unit
         }
 
         [Test]
-        public void CircleAreaTest()
+        public void GetAreaTest()
         {
-            double radius = 1.0d;
-            double actual = Methods.GetArea("Circle", new double[] { radius });
-            double expected = Math.PI * radius * radius;
-            
-            Assert.That(expected, Is.EqualTo(actual).Within(double.Epsilon));
-        }
-
-        [Test]
-        public void CircleAreaWrongArgsCount()
-        {
-            double radius = 1.0d;
-            double otherThing = 1.0d;
-
-            double[] args = new double[] { radius, otherThing };
-
-            Assert.That(Methods.GetArea("Circle", args), Is.EqualTo(-1.0d).Within(double.Epsilon));
-            Assert.That(Methods.GetLastError(), Is.Not.EqualTo(string.Empty));
-        }
-
-        [Test]
-        public void TriangleAreaTest()
-        {
-            double a = 1.0d;
-            double b = 1.0d;
-            double c = 1.0d;
-            double[] sides = new double[] { a, b, c };
-
-            double actual = Methods.GetArea("Triangle", sides);
-            double s = sides.Sum() / 2;
-            double expected = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
-            
-            Assert.That(expected, Is.EqualTo(actual).Within(double.Epsilon));
-        }
-
-        [Test]
-        public void TriangleAreaWrongArgsCount()
-        {
-            double a = 1.0d;
-            double b = 1.0d;
-
-            double[] args = new double[] { a, b };
-
-            Assert.That(Methods.GetArea("Triangle", args), Is.EqualTo(-1.0d).Within(double.Epsilon));
-            Assert.That(Methods.GetLastError(), Is.Not.EqualTo(string.Empty));
+            Methods.Registry.RegisterShape<TestShape>();
+            double area = Methods.GetArea("TestShape", Array.Empty<double>());
+            Assert.That(area, Is.EqualTo(1.0d).Within(double.Epsilon));
         }
     }
 }
--- AreaLib.Unit/ShapeRegistryTests.cs
+++ AreaLib.Unit/ShapeRegistryTests.cs
@@ -6,11 +6,6 @@ using System.Threading.Tasks;
 
 namespace AreaLib.Unit
 {
-    internal class TestShape : Shapes.IShape
-    {
-        public double Area(double[] shapeParams) => -1.0d;
-    }
-
     [TestFixture]
     internal class ShapeRegistryTests
     {
@@ -22,11 +17,11 @@ namespace AreaLib.Unit
 
             registry.RegisterShape<TestShape>();
             Assert.That(registry.GetAvailableShapes()[0], Is.EqualTo("TestShape"));
-            Assert.That(registry.GetArea("TestShape", Array.Empty<double>()), Is.EqualTo(-1.0d));
+            Assert.That(registry.GetArea("TestShape", Array.Empty<double>()), Is.EqualTo(1.0d));
             
             registry.RegisterShape<TestShape>("OtherName");
             Assert.That(registry.GetAvailableShapes()[1], Is.EqualTo("OtherName"));
-            Assert.That(registry.GetArea("OtherName", Array.Empty<double>()), Is.EqualTo(-1.0d));
+            Assert.That(registry.GetArea("OtherName", Array.Empty<double>()), Is.EqualTo(1.0d));
         }
 
         [Test]
