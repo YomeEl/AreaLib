e02ccd6649b614a84ee24c42837e6ffd97feb0cd
Ярослав <umpepatopChannel@gmail.com>
2022-12-03 18:01:24 +0500

    Implement error handling

--- AreaLib/Methods.cs
+++ AreaLib/Methods.cs
@@ -3,7 +3,8 @@
     public static class Methods
     {
         public static ShapeRegistry Registry { get; } = Initializer.InitializeShapeRegistry();
-
+        private static string _lastError = string.Empty;
+        
         public static string[] GetAvailableShapes()
         {
             return Registry.GetAvailableShapes();
@@ -11,12 +12,21 @@
 
         public static double GetArea(string shapeName, double[] shapeParams)
         {
-            return Registry.GetArea(shapeName, shapeParams);
+            _lastError = string.Empty;
+            try
+            {
+                return Registry.GetArea(shapeName, shapeParams);
+            }
+            catch (Exception e)
+            {
+                _lastError = e.Message;
+                return -1;
+            }
         }
 
         public static string GetLastError()
         {
-            throw new NotImplementedException();
+            return _lastError;
         }
     }
 }
